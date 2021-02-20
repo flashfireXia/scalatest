98e6505872b77101778e21a614bf36334d26f914
wangyu17 <wangyu17@kuaishou.com>
2021-02-20 18:47:00 +0800

    add new branch wangyu1

--- src/main/scala/test1.scala
+++ src/main/scala/test1.scala
@@ -5,6 +5,8 @@ class test1 {
 object test1 {
   def main(args: Array[String]): Unit = {
     var str = "Hello World!"
+    var str2 = "I'm in temp branch now."
     println(str)
+    println(str2)
   }
 }
